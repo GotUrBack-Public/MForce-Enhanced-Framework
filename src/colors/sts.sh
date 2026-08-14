7231cfa6c49054dc2bb5e5ff110867267572a0c2
Anonym Arch User <75336897532579754357a@gmail.com>
2026-08-14 07:33:43 +0200

    Add color status variables to sts.sh
    Initialize color status variables for script output.

--- src/colors/sts.sh
+++ src/colors/sts.sh
@@ -1,1 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+COLORS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [[ -z "${CLR_RESET:-}" ]]; then
+    source "$COLORS_DIR/bse.sh"
+fi
+
+STATUS_SUCCESS="${CLR_BRIGHT_GREEN}"
+STATUS_WARNING="${CLR_BRIGHT_YELLOW}"
+STATUS_ERROR="${CLR_BRIGHT_RED}"
+STATUS_INFO="${CLR_BRIGHT_BLUE}"
+STATUS_MUTED="${CLR_GRAY}"
