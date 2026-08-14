7d51b0d49c28749a9a2d7184d0deafc01822c008
Anonym Arch User <75336897532579754357a@gmail.com>
2026-08-14 07:33:10 +0200

    Add color definitions to uic.sh

--- src/colors/uic.sh
+++ src/colors/uic.sh
@@ -1,1 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+COLORS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [[ -z "${CLR_RESET:-}" ]]; then
+    source "$COLORS_DIR/bse.sh"
+fi
+
+UI_TITLE="${CLR_BRIGHT_CYAN}"
+UI_BORDER="${CLR_CYAN}"
+UI_TEXT="${CLR_WHITE}"
+UI_MUTED="${CLR_GRAY}"
+UI_SELECTED="${CLR_BRIGHT_CYAN}"
+UI_SUCCESS="${CLR_BRIGHT_GREEN}"
+UI_WARNING="${CLR_BRIGHT_YELLOW}"
+UI_ERROR="${CLR_BRIGHT_RED}"
+UI_INFO="${CLR_BRIGHT_BLUE}"
