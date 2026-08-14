a0a69cd8a58fc516c54aa9039ac50ee2f87942ea
Anonym Arch User <75336897532579754357a@gmail.com>
2026-08-14 07:34:11 +0200

    Initialize mnu.sh with color variables and sourcing

--- src/colors/mnu.sh
+++ src/colors/mnu.sh
@@ -1,1 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+COLORS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [[ -z "${CLR_RESET:-}" ]]; then
+    source "$COLORS_DIR/bse.sh"
+fi
+
+MENU_SELECTED="${CLR_BRIGHT_CYAN}"
+MENU_NORMAL="${CLR_WHITE}"
+MENU_MUTED="${CLR_GRAY}"
+MENU_KEY="${CLR_BRIGHT_WHITE}"
